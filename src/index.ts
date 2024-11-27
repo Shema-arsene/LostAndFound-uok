232574f50b3ce0594cd2f3a978f086533b1ac3d4
Jovite Kz. Ngoga <kzngoga19@gmail.com>
2024-11-27 20:36:13 +0200

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,21 @@
+import bodyParser from 'body-parser';
+import express from 'express';
+import cors from 'cors';
+import config from './config';
+import dbConnect from './database';
+import routes from './routes';
+
+const app = express();
+dbConnect();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.enable('trust proxy');
+app.use(cors());
+
+app.use('/api/v1', routes);
+
+const port = config.PORT || 5000;
+app.listen(port, () => {
+  console.log(`Server started - URL: http://localhost:${port}/`);
+});
